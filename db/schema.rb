4b860c785a02f19e50d5ae38362ffab404b828bf
ITurres <emanueliturres.co@gmail.com>
2024-01-11 13:50:29 -0300

    Feat: Modify fk constraints on 'category_transaction_details' table
    - Add on delete cascade to 'category' and 'transaction_detail' columns.
    - Re-migrate the table to add the new constraint.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_08_164210) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_11_003500) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_08_164210) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_transaction_details_on_category_id"
     t.index ["user_id"], name: "index_transaction_details_on_user_id"
   end
 
@@ -55,7 +57,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_08_164210) do
   end
 
   add_foreign_key "categories", "users"
-  add_foreign_key "category_transaction_details", "categories"
-  add_foreign_key "category_transaction_details", "transaction_details"
+  add_foreign_key "category_transaction_details", "categories", on_delete: :cascade
+  add_foreign_key "category_transaction_details", "transaction_details", on_delete: :cascade
+  add_foreign_key "transaction_details", "categories"
   add_foreign_key "transaction_details", "users"
 end
